e43c958e8eab8e794b2dc13e3f4939d4ff1fa48c
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2021-09-14 12:18:17 +0200

    Beacon reading process is working.

--- app/src/main/java/com/example/challenge2/SensorActivity.java
+++ app/src/main/java/com/example/challenge2/SensorActivity.java
@@ -1,14 +1,11 @@
 package com.example.challenge2;
 
 import android.Manifest;
-import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -31,20 +28,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.RegionViewModel;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static android.view.View.GONE;
 
-
-public class SensorActivity extends FragmentActivity implements SensorEventListener, OnMapReadyCallback, LocationListener, View.OnClickListener, RangeNotifier, MonitorNotifier {
+// View.OnClickListener, BeaconConsumer, RangeNotifier
+public class SensorActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, View.OnClickListener, RangeNotifier, BeaconConsumer {
 
     // sending log output TAG
     private static final String TAG = "MyActivity";
@@ -53,8 +50,9 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
     private BeaconManager beaconManager;
 
     // Search for beacons
-    private static Region region = new Region("rid", null, null, null);
+    private static Region region;
 
+    private static final long SCAN_TIME = 6000l;
     private static final String BECONS_CLOSE_BY = "BeaconsCloseBy";
 
     // Program is running or is on pause
@@ -91,7 +89,6 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
         introText1 = findViewById(R.id.introText1);
         introText2 = findViewById(R.id.introText2);
         introText3 = findViewById(R.id.introText3);
-
         startButton = findViewById(R.id.startButton);
         again = findViewById(R.id.again);
         linearLayout = findViewById(R.id.layout);
@@ -101,19 +98,20 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
         // Initialize Beacon manager
         beaconManager = BeaconManager.getInstanceForApplication(this);
 
-        // Set a beacon protocol; Eddystone. We can choose a diferent one later if we want
-        // Another example would be iBeacon or Kontakt.io. Search on google as it is not included in this library
+        // Set a beacon protocol
+        // Another example would be Kontakt.io. https://beaconlayout.wordpress.com/
+        // Eddystone UID and Eddystone TLM
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        // In case we would like iBeacon
-
-        /*beaconManager.getBeaconParsers().add(
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+        // iBeacon
+        beaconManager.getBeaconParsers().add(
                 new BeaconParser().
-                        setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));*/
+                        setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-        // Beacon identifiers
+        // Beacon identifiers. Beacons with all possible identifiers (empty arrayList)
         ArrayList<Identifier> identifiers = new ArrayList<>();
 
-        region = new Region("regionId", null, null, null);
+        region = new Region(BECONS_CLOSE_BY, identifiers);
 
         // ----------------------------------------
 
@@ -123,8 +121,8 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
         screenReceiver = new ScreenReceiver();
         registerReceiver(screenReceiver,filter);
 
-        if (ActivityCompat.checkSelfPermission((Activity) this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity) this, new String[]{
+        if (ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION
             }, 10);
         }
@@ -150,58 +148,105 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
     @Override
     public void onClick(View view){
         // Press button to start application
+        Log.d(TAG, "has entered onClick");
         if (view.equals(startButton) && !running){
-            if (ActivityCompat.checkSelfPermission((Activity) this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions((Activity) this, new String[]{
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{
                         android.Manifest.permission.ACCESS_FINE_LOCATION
                 }, 10);
-                StartDetectingBeacons();
             }
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (bluetoothAdapter.isEnabled()){
+                StartDetectingBeacons();
+            } else {
+                // Ask the user to enable bluetooth
+                Intent ask_bluetooth_enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(ask_bluetooth_enable, 1);
+            }
+
         // Press button to pause application
         } else if (view.equals(startButton) && running){
             StopDetectingBeacons();
         }
     }
 
+
     private void StartDetectingBeacons() {
+        Log.d(TAG, "has entered StartDetectingBeacons");
 
         // App is now running (detecting beacons)
         running = true;
 
+        // Change the icon to the pause symbol
+        startButton.setImageResource(R.drawable.stop);
+
         // Time to scan for beacons (6 seconds)
-        beaconManager.setForegroundBetweenScanPeriod(6000l);
+        beaconManager.setForegroundBetweenScanPeriod(SCAN_TIME);
 
-        // Start to search for beacons
+        // Pair with beacon service
+        beaconManager.bind(this);
+
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+
         try {
-            // Monitoring (look for beacons that match the passed Region object)
-            // Ranging ( monitor and provide updates on the est. mDistance every seconds while beacons are visible)
-            beaconManager.startMonitoring(region);
-            beaconManager.startRangingBeacons(region);
-            beaconManager.addMonitorNotifier(this);
-
-            // Set up a Live Data observer
-            RegionViewModel regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(region);
-
-
-        Log.d(TAG, "Searching for beacons");
-
+            // Start to search for beacons
+            beaconManager.startRangingBeaconsInRegion(region);
+            Log.d(TAG, "Searching for beacons");
         } catch (Exception e){
             Log.d(TAG, "An error occured while trying to search for beacons" + e.getMessage());
         }
 
         // Display beacons detected after the time given to scan (6 seconds in our case)
-            // setMonitoringListener instead of setRangingListener
+        beaconManager.addRangeNotifier(this);
 
+    }
 
-        //if (beacons.size() != 0){
+    /**
+     * Called after SCAN_TIME seconds with the detected beacons in that time period
+     */
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
 
-       // }
+        if (beacons.size() != 0){
+            for (Beacon beacon : beacons){
+                Log.d(TAG, "Beacon detected: " + beacon);
+            }
+        } else {
+            Log.d(TAG, "No beacons were detected");
+        }
 
     }
 
     private void StopDetectingBeacons() {
+
+        // App is not running (stop detecting beacons)
         running = false;
-        beaconManager.stopRangingBeacons(region);
+
+        // Change the icon to the start symbol
+        startButton.setImageResource(R.drawable.play);
+
+        // Hide icon
+        startButton.setVisibility(View.GONE);
+
+        // Stop the process of detection
+        try {
+            beaconManager.stopMonitoringBeaconsInRegion(region);
+            Log.d(TAG, "Stop detecting beacons...");
+        } catch (Exception e){
+            Log.d(TAG, "An error occured while trying to stop detecting beacons" + e.getMessage());
+        }
+
+        beaconManager.removeAllRangeNotifiers();
+        beaconManager.unbind(this);
+
+        // TODO: Buttons and text gone, Map appears with current location
+        // Todo: Maybe it is an idea to first check if someone is inside a building or outside
+        // If inside, then use beacons. Else, use gps.
+
     }
 
 
@@ -211,29 +256,12 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
             lat = location.getLatitude();
             longi = location.getLongitude();
             googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, longi)));
-            // lat_total.add(lat);
-            // longi_total.add(longi);
             requested = false;
         }
         if (mapView.getVisibility() == View.VISIBLE) {
             float zoomLevel = 15f;
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, longi), zoomLevel));
-            // GET THE ONES FROM THE CLOUD AND CHANGE THEIR COLOR TO BLUE?
-
-
-            // SEND ALL THE LAT AND LONGI TO THE CLOUD
-
         }
-    }
-
-
-    @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-    }
-
-    @Override
-    public final void onSensorChanged(SensorEvent event) {
     }
 
     @Override
@@ -259,7 +287,7 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
         mapView.onResume();
         mapView.onEnterAmbient(null);
         // This view is invisible, but it still takes up space for layout purposes. Otherwise use GONE
-        mapView.setVisibility(View.GONE);
+        mapView.setVisibility(GONE);
     }
 
     @Override
@@ -277,24 +305,4 @@ public class SensorActivity extends FragmentActivity implements SensorEventListe
 
     }
 
-
-    @Override
-    public void didEnterRegion(Region region) {
-
-    }
-
-    @Override
-    public void didExitRegion(Region region) {
-
-    }
-
-    @Override
-    public void didDetermineStateForRegion(int state, Region region) {
-
-    }
-
-    @Override
-    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-
-    }
 };
